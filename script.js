836d0995667dfc108836fd3b216feb267b02866e
borchiajeremias <borchiajeremias@gmail.com>
2026-05-27 06:27:39 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,50 @@
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener("click", function(e){
+
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute("href"))
+        .scrollIntoView({
+            behavior: "smooth"
+        });
+
+    });
+});
+
+const topBtn = document.getElementById("topBtn");
+
+window.addEventListener("scroll", () => {
+
+    if(window.scrollY > 300){
+        topBtn.style.display = "block";
+    }else{
+        topBtn.style.display = "none";
+    }
+
+});
+
+topBtn.addEventListener("click", () => {
+
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+
+});
+
+const sections = document.querySelectorAll("section");
+
+window.addEventListener("scroll", () => {
+
+    sections.forEach(section => {
+
+        const position = section.getBoundingClientRect().top;
+
+        if(position < window.innerHeight - 100){
+            section.classList.add("show");
+        }
+
+    });
+
+});
+
